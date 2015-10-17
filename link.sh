b7bbd81f75e2061f248ebc9f1515d55414cb033f
Danny Nemer <hi@DannyNemer.com>
2015-10-17 11:56:53 -0400

    mackup: Add `mackup`
    Adds `mackup` installation, configuration files, and symbol link
    creations.

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 # Create symbolic links in the home directory to the dotfiles.
-for file in $(cd $(dirname $0) && pwd)/.{zshrc,aliases,exports,functions,vimrc,gitignore,gitconfig,tern-config}; do
+for file in $(cd $(dirname $0) && pwd)/.{zshrc,aliases,exports,functions,vimrc,gitignore,gitconfig,tern-config,mackup,mackup.cfg}; do
 	ln -sv $file ~
 done
