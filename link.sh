902d5166a4813e7f96959db464b3c1b206839f26
Danny Nemer <hi@DannyNemer.com>
2016-06-01 18:32:38 -0400

    fit-commit,link: Add `fit-commit`
    Adds `.fit_commit.yml`. Extends `link.sh` to create a symbolic link in
    the home directory to the file.

--- link.sh
+++ link.sh
@@ -1,4 +1,4 @@
 # Create symbolic links in the home directory to the dotfiles.
-for file in $(cd $(dirname $0) && pwd)/.{zshrc,aliases,exports,functions,vimrc,gitignore,gitconfig,tern-config,mackup,mackup.cfg}; do
+for file in $(cd $(dirname $0) && pwd)/.{zshrc,aliases,exports,functions,vimrc,gitignore,gitconfig,tern-config,mackup,mackup.cfg,fit_commit.yml}; do
 	ln -sv $file ~
 done
