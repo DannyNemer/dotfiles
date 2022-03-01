52f40755cfdc317485a7dd92462e27db00fa5104
Danny Nemer <hi@DannyNemer.com>
2022-02-28 16:37:24 -0800

    install: Update lists of brew recipes

--- install.sh
+++ install.sh
@@ -3,17 +3,52 @@
 # Install Xcode command line tools. Must be first installation.
 xcode-select --install
 
-# Install Oh-My-Zsh.
+# Install Oh-My-Zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
-# Install Homebrew.
+# Install Homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install GUI programs.
-brew install 1password alfred appcleaner bettertouchtool dash docker dropbox font-menlo-for-powerline gitup google-chrome istat-menus iterm2-beta kaleidoscope postgres rescuetime slack spotify tableplus visual-studio-code zoomus
+# Install GUI programs
+brew install \
+    1password \
+    alfred \
+    appcleaner \
+    bettertouchtool \
+    dash \
+    docker \
+    dropbox \
+    fig \
+    font-menlo-for-powerline \
+    gitup \
+    google-chrome \
+    istat-menus \
+    iterm2-beta \
+    kaleidoscope \
+    linear-linear \
+    notion \
+    postgres-unofficial \
+    rescuetime
+    slack \
+    spotify \
+    tableplus \
+    visual-studio-code \
+    zoom
 
-# Install Homebrew packages.
-brew install coreutils diff-so-fancy fasd fzf htop node python trash yarn zsh-autosuggestions zsh-syntax-highlighting
+# Install Homebrew packages
+brew install \
+    python \
+    node \
+    yarn \
+    git \
+    gh \
+    cloc \
+    coreutils \
+    fasd \
+    fzf \
+    zsh-autosuggestions \
+    zsh-syntax-highlighting \
+    zsh-completions
 
-# Install `fzf` shell extensions.
+# Install `fzf` shell extensions
 /usr/local/opt/fzf/install
