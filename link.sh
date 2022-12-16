d8e4a22595267fd9eb8f779c60395a2944e59fc5
Danny Nemer <hi@DannyNemer.com>
2022-12-16 13:59:52 -0800

    Remove `.p10k.zsh` from `link.sh`

--- link.sh
+++ link.sh
@@ -1,5 +1,5 @@
 # Create symbolic links in the home directory to the dotfiles.
-for file in $(cd $(dirname $0) && pwd)/.{zshrc,p10k.zsh,gitconfig}; do
+for file in $(cd $(dirname $0) && pwd)/.{zshrc,gitconfig}; do
 	ln -sv $file ~
 done
 
