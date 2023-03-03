89845ddc5994deef20ccf851d3d5f537e62b0f50
Danny Nemer <hi@DannyNemer.com>
2023-03-03 12:20:35 -0800

    Convert indentation to spaces in `link.sh`

--- link.sh
+++ link.sh
@@ -1,6 +1,6 @@
 # Create symbolic links in the home directory to the dotfiles.
 for file in $(cd $(dirname $0) && pwd)/.{zshrc,gitconfig}; do
-	ln -sv $file ~
+  ln -sv $file ~
 done
 
 # Sync the desktop folder with Dropbox.
